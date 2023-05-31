d94d7d486070aba6c122817063769f9bee452bad
Andres Arcila <andresarcila2@me.com>
2023-05-30 21:52:50 -0500

    getall products products

--- src/routes/shop/index.tsx
+++ src/routes/shop/index.tsx
@@ -1,10 +1,31 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { Resource, component$, useResource$ } from '@builder.io/qwik';
+import { DocumentHead, routeLoader$, server$ } from '@builder.io/qwik-city';
+import { Product } from '@prisma/client';
+import { ProductService } from '~/services/ProductService';
+const productService = new ProductService();
+
+export const useGetProducts = routeLoader$(async () => {
+  return await productService.getProducts();
+});
 
 export default component$(() => {
+  const products = useResource$(async ({ track }) => {
+    return await productService.getProducts();
+  }); 
   return (
     <>
       <h1>Shop</h1>
+      <Resource
+        value={products}
+        onPending={() => <p>Loading...</p>}
+        onResolved={(products) => (
+          <>
+            {products.map((product, i) => (
+              <p key={i}>{product.name}</p>
+            ))}
+          </>
+        )}
+      />
     </>
   );
 });
--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -3,38 +3,43 @@ import { PrismaClient, Product } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export class ProductService {
-  async createProduct(productData: Omit<Product, "id">): Promise<Product> {
-    const product = await prisma.product.create({
-      data: productData,
-    });
-    return product;
-  }
+    async getProducts(): Promise<Product[]> {
+        const product = await prisma.product.findMany();
+        return product;
+    }
+      
+    async createProduct(productData: Omit<Product, "id">): Promise<Product> {
+        const product = await prisma.product.create({
+        data: productData,
+        });
+        return product;
+    }
 
-  async getProductById(productId: number): Promise<Product | null> {
-    const product = await prisma.product.findUnique({
-      where: {
-        id: productId,
-      },
-    });
-    return product;
-  }
+    async getProductById(productId: number): Promise<Product | null> {
+        const product = await prisma.product.findUnique({
+        where: {
+            id: productId,
+        },
+        });
+        return product;
+    }
 
-  async updateProduct(productId: number, productData: Partial<Product>): Promise<Product | null> {
-    const product = await prisma.product.update({
-      where: {
-        id: productId,
-      },
-      data: productData,
-    });
-    return product;
-  }
+    async updateProduct(productId: number, productData: Partial<Product>): Promise<Product | null> {
+        const product = await prisma.product.update({
+        where: {
+            id: productId,
+        },
+        data: productData,
+        });
+        return product;
+    }
 
-  async deleteProduct(productId: number): Promise<Product | null> {
-    const product = await prisma.product.delete({
-      where: {
-        id: productId,
-      },
-    });
-    return product;
-  }
+    async deleteProduct(productId: number): Promise<Product | null> {
+        const product = await prisma.product.delete({
+        where: {
+            id: productId,
+        },
+        });
+        return product;
+    }
 }
