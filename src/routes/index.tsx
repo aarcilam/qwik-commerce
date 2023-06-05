2ec79062b6573d06a5f44f19361a4229e3cf9683
Andres Arcila <andresarcila2@me.com>
2023-06-05 09:14:32 -0500

    nav with tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,19 +3,19 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <>
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <h1 class=" text-4xl">Hi 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </p>
-    </>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Shop',
   meta: [
     {
       name: 'description',
