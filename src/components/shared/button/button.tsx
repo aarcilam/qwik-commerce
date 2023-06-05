27b0e092c0b334a981f572ff01319244c3cec41e
Andres Arcila <andresarcila2@me.com>
2023-06-05 09:20:53 -0500

    styles in home

--- src/components/shared/button/button.tsx
+++ src/components/shared/button/button.tsx
@@ -7,6 +7,6 @@ export interface ButtonProps {
 
 export const Button = component$<ButtonProps>((props) => {
   return (
-    <button type="submit" onClick$={props.onClick$}> {props.text} </button>
+    <button class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit" onClick$={props.onClick$}> {props.text} </button>
   );
 });
