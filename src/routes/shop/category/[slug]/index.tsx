8c378ce3b9fa8ae10a5dd8813ef5274ec64894b0
Andres Arcila <andresarcila2@me.com>
2023-06-06 22:13:19 -0500

    correction

--- src/routes/shop/category/[slug]/index.tsx
+++ src/routes/shop/category/[slug]/index.tsx
@@ -9,10 +9,13 @@ export default component$(() => {
     const {getCategoryProducts} = useCategories();
     const products:Signal<Product[]> = useSignal([]);
     useTask$(async ({track})=>{
-        const productsResponse = await getCategoryProducts(+loc.params.slug);
-        if(productsResponse) products.value = [...productsResponse]
+        track(()=>loc.params.slug)
+        if(loc.params.slug){
+            const productsResponse = await getCategoryProducts(+loc.params.slug);
+            if(productsResponse) products.value = [...productsResponse]
+        }
     })
     return (<>
-        <ProductsCollection products={products.value} />
+        {products.value.length>0 && <ProductsCollection products={products.value} />}
     </>)
 });
