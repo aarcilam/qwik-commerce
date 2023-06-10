ed850fe416426a8059ef09388ae3ee1c4eacc9e9
Andres Arcila <andresarcila2@me.com>
2023-06-10 10:33:26 -0500

    checkout form

--- src/routes/shop/checkout/index.tsx
+++ src/routes/shop/checkout/index.tsx
@@ -1,11 +1,13 @@
 import { component$ } from "@builder.io/qwik";
 import { routeLoader$, z, type DocumentHead } from "@builder.io/qwik-city";
-import { InitialValues, formAction$, zodForm$ } from "@modular-forms/qwik";
+import { InitialValues, formAction$, zodForm$, useForm } from "@modular-forms/qwik";
 import { User } from "@prisma/client";
+import { ButtonInput } from "~/components/shared/forms/button-input/button-input";
+import { TextInput } from "~/components/shared/forms/text-input/text-input";
 import { LoggedUsedResponseData } from "~/routes/auth/register";
 import { UserService } from "~/services/UserService";
 
-const shippingSchema = z.object({
+const checkoutSchema = z.object({
   email: z
     .string()
     .min(1, "Please enter your email.")
@@ -40,9 +42,9 @@ const shippingSchema = z.object({
     .min(1, "Please enter your phone."),
 });
 
-type ShippingForm = z.infer<typeof shippingSchema>;
+type CheckoutForm = z.infer<typeof checkoutSchema>;
 
-export const useFormLoader = routeLoader$<InitialValues<ShippingForm>>(() => ({
+export const useFormLoader = routeLoader$<InitialValues<CheckoutForm>>(() => ({
   email: "",
   country: "",
   name: "",
@@ -52,21 +54,129 @@ export const useFormLoader = routeLoader$<InitialValues<ShippingForm>>(() => ({
   city: "",
   department: "",
   postalCode: "",
-  phone:""
+  phone: ""
 }));
 
-export const useFormAction = formAction$<ShippingForm, LoggedUsedResponseData>(
+export const useFormAction = formAction$<CheckoutForm>(
   async (values) => {
     // Runs on server
 
   },
-  zodForm$(shippingSchema)
+  zodForm$(checkoutSchema)
 );
 
 export default component$(() => {
+  const [shippingForm, { Form, Field }] = useForm<CheckoutForm>({
+    loader: useFormLoader(),
+    action: useFormAction(),
+    validate: zodForm$(checkoutSchema),
+  });
   return (
     <>
       <h1>Checkout</h1>
+      <Form>
+        <Field name="email">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="email"
+              field={field}
+              placeholder="Insert a email"
+            />
+          )}
+        </Field>
+        <Field name="country">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="country"
+              field={field}
+              placeholder="Insert a country"
+            />
+          )}
+        </Field>
+        <Field name="name">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="name"
+              field={field}
+              placeholder="Insert a name"
+            />
+          )}
+        </Field>
+        <Field name="surname">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="surname"
+              field={field}
+              placeholder="Insert a surname"
+            />
+          )}
+        </Field>
+        <Field name="address">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="address"
+              field={field}
+              placeholder="Insert a address"
+            />
+          )}
+        </Field>
+        <Field name="addressComplement">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="addressComplement"
+              field={field}
+              placeholder="Insert a addressComplement"
+            />
+          )}
+        </Field>
+        <Field name="city">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="city"
+              field={field}
+              placeholder="Insert a city"
+            />
+          )}
+        </Field>
+        <Field name="department">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="department"
+              field={field}
+              placeholder="Insert a department"
+            />
+          )}
+        </Field>
+        <Field name="postalCode">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="postalCode"
+              field={field}
+              placeholder="Insert a postalCode"
+            />
+          )}
+        </Field>
+        <Field name="phone">
+          {(field, props) => (
+            <TextInput
+              props={props}
+              type="phone"
+              field={field}
+              placeholder="Insert a phone"
+            />
+          )}
+        </Field>
+        <ButtonInput value="Go to shipping" />
+      </Form>
     </>
   );
 });
