5ff2848662d041bf5793cf5263e6e079202ea047
Andres Arcila <andresarcila2@me.com>
2023-06-11 12:44:41 -0500

    basic order structure

--- src/components/shared/forms/select/select.tsx
+++ src/components/shared/forms/select/select.tsx
@@ -1,20 +1,26 @@
 import { QRL, component$ } from "@builder.io/qwik";
+import { FieldStore } from "@modular-forms/qwik";
 
 export interface SelectProps {
-  type: string;
+  type?: string;
   options: string[];
   onChange$?: QRL;
+  props?: {},
+  field?: FieldStore<any, any>;
 }
 
 export const Select = component$<SelectProps>((props) => {
   return (
-    <select name="" id="" onChange$={props.onChange$}>
-      <option disabled selected>
-        Select a value
-      </option>
-      {props.options.map((option) => (
-        <option value={option}>{option}</option>
-      ))}
-    </select>
+    <>
+      <select {...props.props} onChange$={props.onChange$}>
+        <option disabled selected>
+          Select a value
+        </option>
+        {props.options.map((option) => (
+          <option value={option} selected={props.field?.value === option}>{option}</option>
+        ))}
+      </select>
+      {props.field?.error && <div>{props.field.error}</div>}
+    </>
   );
 });
--- src/routes/shop/checkout/shippings/index.tsx
+++ src/routes/shop/checkout/shippings/index.tsx
@@ -1,17 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import { routeLoader$, z, type DocumentHead } from "@builder.io/qwik-city";
 import { InitialValues, formAction$, zodForm$, useForm } from "@modular-forms/qwik";
-import { User } from "@prisma/client";
 import { ButtonInput } from "~/components/shared/forms/button-input/button-input";
+import { Select } from "~/components/shared/forms/select/select";
 import { TextInput } from "~/components/shared/forms/text-input/text-input";
-import { LoggedUsedResponseData } from "~/routes/auth/register";
-import { UserService } from "~/services/UserService";
 
 const shippingsSchema = z.object({
   shippingMethod: z
     .string()
     .min(1, "Please enter your shipping method.")
-    .email("The email address is badly formatted."),
 });
 
 type ShippingsForm = z.infer<typeof shippingsSchema>;
@@ -40,12 +37,7 @@ export default component$(() => {
       <Form>
         <Field name="shippingMethod">
           {(field, props) => (
-            <TextInput
-              props={props}
-              type="shippingMethod"
-              field={field}
-              placeholder="Insert a shippingMethod"
-            />
+            <Select props={props} field={field} options={['Free Shipping','International']} ></Select>
           )}
         </Field>
         <ButtonInput value="Go to payment" />
