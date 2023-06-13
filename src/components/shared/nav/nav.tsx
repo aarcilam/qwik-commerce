406df83ffcbd00713b9a0328869ecfb385248d34
Andres Arcila <andresarcila2@me.com>
2023-06-12 23:38:35 -0500

    responsive menu

--- src/components/shared/nav/nav.tsx
+++ src/components/shared/nav/nav.tsx
@@ -16,20 +16,36 @@ export const Nav = component$(() => {
     if (!checked) theme.value = "winter";
   });
 
-  useTask$(({track})=>{
-    track(()=>cart.cart.lastAdded)
+  useTask$(({ track }) => {
+    track(() => cart.cart.lastAdded)
     cartRef.value?.focus();
   })
   return (
     <div class="navbar bg-base-100 fixed w-full top-0 left-0 z-40">
-      <div class="flex-1">
-        <Link href="/" class="btn btn-ghost normal-case text-xl">
-          <img
-            class="h-8 w-auto"
-            src="https://s3-alpha.figma.com/hub/file/2649946739/f6a7462a-0e1d-42d2-81aa-548a5dbce420-cover.png"
-            alt=""
-          />
-        </Link>
+      <div class="drawer flex-1 block md:hidden">
+        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+          <label for="my-drawer" class="btn btn-circle swap swap-rotate drawer-button">
+            <input type="checkbox" />
+            <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+            <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
+          </label>
+        </div>
+        <div class="drawer-side">
+          <label for="my-drawer" class="drawer-overlay"></label>
+          <div class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            <Menu />
+          </div>
+        </div>
+      </div>
+      <Link href="/" class="btn btn-ghost normal-case text-xl">
+        <img
+          class="h-8 w-auto"
+          src="https://s3-alpha.figma.com/hub/file/2649946739/f6a7462a-0e1d-42d2-81aa-548a5dbce420-cover.png"
+          alt=""
+        />
+      </Link>
+      <div class="flex-1 hidden md:block">
         <Menu />
       </div>
       <div class="flex-none">
